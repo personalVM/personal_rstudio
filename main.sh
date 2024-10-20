ce91e6b1af9f9fa5c2a715522049271268e76bae
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-10-21 00:05:20 +0200

    edit4

--- main.sh
+++ main.sh
@@ -2,5 +2,7 @@ sudo docker build -t personal_rstudio_img:v20241020 -f rstudio.Dockerfile .
 # sudo docker images
 sudo docker inspect personal_rstudio_img:v20241020 --format '{{ .ID }}' | cut -f2- -d:
 personal_rstudio_img_id=$(sudo docker inspect personal_rstudio_img:v20241020 --format '{{ .ID }}' | cut -f2- -d:)
-sudo docker run -d -e PASSWORD=minhasenha -p 8787:8787 -v $(pwd):/home/rstudio/my_data --name=personal_rstudio_ctn4 $personal_rstudio_img_id
+sudo docker run -d -e PASSWORD=minhasenha -p 8787:8787 -v $(pwd)/volume/:/home/rstudio/volume --name=personal_rstudio_ctn4 $personal_rstudio_img_id
 docker ps
+
+
