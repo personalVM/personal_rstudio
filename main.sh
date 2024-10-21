9031316e875698270ce318e898bd01b6b1fd38c9
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-10-21 02:33:11 +0200

    edit12

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ img_tag=${2:-personal_rstudio_img:v20241020}  # Second argument: Image tag (defa
 container_name=${3:-personal_rstudio_ctn}  # Third argument: Container name (default: personal_rstudio_ctn)
 password=${4:-minhasenha}  # Fourth argument: RStudio password (default: minhasenha)
 port=${5:-8787}  # Fifth argument: Port (default: 8787)
-volume_dir=${6:-$(pwd)}  # Sixth argument: Volume directory (default: current directory/volume)
+volume_dir=${6:-$(pwd)/}  # Sixth argument: Volume directory (default: current directory/volume)
 
 # Step 1: Build the Docker image
 sudo docker build -t "$img_tag" -f "$dockerfile" .
@@ -15,7 +15,7 @@ sudo docker build -t "$img_tag" -f "$dockerfile" .
 img_id=$(sudo docker inspect "$img_tag" --format '{{ .ID }}' | cut -f2- -d:)
 
 # Step 3: Run a Docker container with the specified image
-sudo docker run -d -e PASSWORD="$password" -p "$port":8787 -v "$volume_dir":/home/rstudio/volume --name="$container_name" "$img_id"
+sudo docker run -d -e PASSWORD="$password" -p "$port":8787 -v "$volume_dir":/home/rstudio/ --name="$container_name" "$img_id"
 
 # Step 4: List running containers
 sudo docker ps
