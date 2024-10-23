d9306f8debceb197f8884461e07e3890c37fafaf
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-10-23 21:24:26 +0200

    edit23

--- main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker build -t personal_rstudio_img:v20241020 -f /home/guilhermeviegas1993/personal_rstudio/rstudio.Dockerfile .
-export img_id=$(sudo docker inspect personal_rstudio_img:v20241020 --format '{{ .ID }}' | cut -f2- -d:)
-sudo docker-compose -f /home/guilhermeviegas1993/personal_rstudio/docker-compose.yml up -d --build
-sudo docker ps
