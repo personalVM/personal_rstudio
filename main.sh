d3026dd0c3cdae75cb646c323cd133346ef6bbc7
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-10-23 18:40:11 +0200

    lets try absolute path

--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ sudo docker build -t personal_rstudio_img:v20241020 -f /home/guilhermeviegas1993
 img_id=$(sudo docker inspect personal_rstudio_img:v20241020 --format '{{ .ID }}' | cut -f2- -d:)
 
 # Step 3: Run a Docker container with the specified image
-sudo docker run -d -e PASSWORD="$password" -p 8787:8787 -v ..:/home/rstudio/ --name=personal_rstudio_ctn "$img_id"
+sudo docker run -d -e PASSWORD="$password" -p 8787:8787 -v /home/guilhermeviegas1993/:/home/rstudio/ --name=personal_rstudio_ctn "$img_id"
 
 # Step 4: List running containers
 sudo docker ps
